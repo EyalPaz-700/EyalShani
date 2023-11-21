1d3004d98ea74e7fc0f09bdb9694ebd32fc589b3
shanimashmush <shanimashmush@hilma.tech>
2023-11-21 16:50:45 +0200

    add imports

--- EyalShaniClient/src/App.jsx
+++ EyalShaniClient/src/App.jsx
@@ -1,4 +1,8 @@
 import Todos from "./components/Todos";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-broser-route";
 function App() {
   return (
     <>
@@ -7,6 +11,7 @@ function App() {
           <Route path="/Login" element={<Login />} />
           <Route path="/Home" element={<Home />} />
           <Route path="/Signup" element={<Signup />} />
+          <Route path="/Todos" element={<Todos />} />
         </Routes>
       </BrowserRouter>
     </>
